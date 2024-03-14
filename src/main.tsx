b7d6a089f906fdc870768dfcd7044d028d69c48f
back_ky <black13131303@gmail.com>
2024-03-14 15:32:37 +0700

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-<<<<<<< HEAD
-import { DatePicker } from 'antd';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App/>
-=======
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
->>>>>>> origin/dev
   </React.StrictMode>,
 )
